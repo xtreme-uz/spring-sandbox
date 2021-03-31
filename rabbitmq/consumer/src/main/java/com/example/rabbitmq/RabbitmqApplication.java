43b06be7c79f8cdcb1ce1b196a7f0ab027a2a501
rustambekov <rustambekov.mail.box@gmail.com>
2021-03-31 14:27:42 +0500

    feat: rabbit consumer

--- rabbitmq/consumer/src/main/java/com/example/rabbitmq/RabbitmqApplication.java
+++ rabbitmq/consumer/src/main/java/com/example/rabbitmq/RabbitmqApplication.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,35 +12,37 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class RabbitmqApplication {
 
-	public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+	@Value("${amqp.queue.name}")
+	private String queueName;
 
-	public static final String QUEUE_NAME = "spring-boot";
+	@Value("${amqp.exchange.name}")
+	private String exchangeName;
 
 	@Bean
 	public Queue queue() {
-		return new Queue(QUEUE_NAME, false);
+		return new Queue(queueName, false);
 	}
 
 	@Bean
 	public TopicExchange exchange() {
-		return new TopicExchange(TOPIC_EXCHANGE_NAME);
+		return new TopicExchange(exchangeName);
 	}
 
 	@Bean
 	public Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
 	}
 
 	@Bean
-	public MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
+	public MessageListenerAdapter listenerAdapter(DataConsumer dataConsumer) {
+		return new MessageListenerAdapter(dataConsumer, "receiveMessage");
 	}
 
 	@Bean
 	public SimpleMessageListenerContainer container(ConnectionFactory factory, MessageListenerAdapter listener) {
 		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 		container.setConnectionFactory(factory);
-		container.setQueueNames(QUEUE_NAME);
+		container.setQueueNames(queueName);
 		container.setMessageListener(listener);
 		return container;
 	}
