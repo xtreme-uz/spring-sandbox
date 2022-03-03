d3309b3b5c683672b3ccf5e1518c49e42ddd6225
rustambekov <rustambekov.mail.box@gmail.com>
2022-03-03 14:31:14 +0500

    exp: data mongodb

--- data-mongodb/src/main/java/uz/xtreme/mongodb/Student.java
+++ data-mongodb/src/main/java/uz/xtreme/mongodb/Student.java
@@ -1,28 +1,16 @@
 package uz.xtreme.mongodb;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-/**
- * Author: Rustambekov Avazbek
- * Date: 06/07/2020
- * Time: 16:15
- */
-
-@Data
-@Builder
-@AllArgsConstructor
+@Getter
+@Setter
 @Document
-@ToString
 public class Student {
-
     @Id
     private String id;
     private String firstName;
     private String lastName;
-
 }
--- data-mongodb/src/test/java/uz/xtreme/mongodb/DataMongodbApplicationTests.java
+++ data-mongodb/src/test/java/uz/xtreme/mongodb/DataMongodbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExampleApplicationTests {
+class DataMongodbApplicationTests {
 
 	@Test
 	void contextLoads() {
