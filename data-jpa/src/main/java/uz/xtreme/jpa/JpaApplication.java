ae1316f2f6cf4023762a7fe0f821087bc7e9c35f
rustambekov <rustambekov.mail.box@gmail.com>
2022-04-07 16:19:34 +0500

    fix: removed unnecessary class

--- data-jpa/src/main/java/uz/xtreme/jpa/JpaApplication.java
+++ data-jpa/src/main/java/uz/xtreme/jpa/JpaApplication.java
@@ -1,13 +1,26 @@
 package uz.xtreme.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import uz.xtreme.jpa.domain.Community;
+import uz.xtreme.jpa.repository.CommunityRepository;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	//save child records when parent record is deleted
+	@Bean
+	CommandLineRunner parentChildAssociation(CommunityRepository parentRepository) {
+		return args -> {
+			Community parent = parentRepository.findCommunityById(1L).orElseThrow();
+			parentRepository.delete(parent);
+		};
 	}
 
 }
