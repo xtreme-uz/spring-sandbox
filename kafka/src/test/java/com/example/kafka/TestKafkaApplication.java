10f52b2d95ef94975b3fcf1226c10ea37864bca1
xtreme-uz <rustambekov.mail.box@gmail.com>
2023-11-15 18:13:59 +0300

    feat: kafka sample

---
+++ kafka/src/test/java/com/example/kafka/TestKafkaApplication.java
@@ -0,0 +1,23 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestKafkaApplication {
+
+	@Bean
+	@ServiceConnection
+	KafkaContainer kafkaContainer() {
+		return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(KafkaApplication::main).with(TestKafkaApplication.class).run(args);
+	}
+
+}
